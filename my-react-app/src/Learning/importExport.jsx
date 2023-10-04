93cc16000bd724dc2f85f0aea45819faae049fc1
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-10-04 15:24:55 +0500

    Learning

---
+++ my-react-app/src/Learning/importExport.jsx
@@ -0,0 +1,7 @@
+const name = "Umair Aftab";
+const age = 20;
+function Program() {
+  return "react";
+}
+export default name;
+export { age, Program };
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import name, { Program, age } from "./Learning/importExport";
+import Car from "./Learning/component";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const myFirstElement = <h1>Hello React!</h1>;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(myFirstElement);
+root.render(name);
+root.render(age);
+root.render(Program());
+root.render(Car());
