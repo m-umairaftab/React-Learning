45cbe3c2c056bef3d112a580bce6aaaa2bce72a9
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-10-26 11:46:32 +0500

    tutorials

---
+++ tutorials/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Component from "./tuts/Component";
+
+function App() {
+  return (
+    <>
+      <Component />
+      <Component />
+      <Component />
+    </>
+  );
+}
+
+export default App;
---
+++ tutorials/src/tuts/Component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Component() {
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <h1>This is component example</h1>
+    </div>
+  );
+}
+
+export default Component;
